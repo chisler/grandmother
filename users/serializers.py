fd8fafcf51e5f9bf83352af43709693f9c50440c
Alexander Chisler <alexander.chisler@gmail.com>
2018-07-22 00:10:38 +0100

    profile by user id

--- users/serializers.py
+++ users/serializers.py
@@ -53,7 +53,7 @@ def get_change(current, previous):
     if current == previous:
         return 100.0
     try:
-        return (abs(current - previous) / previous) * 100.0
+        return ((current - previous) / previous) * 100.0
     except ZeroDivisionError:
         return 0
 
